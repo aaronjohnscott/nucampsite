e007d10f3fb760d5d56caa32560d9583b49f8a79
Aaron Scott <aaronjohnscott@gmail.com>
2023-03-06 11:10:38 -0800

    from laptop

---
+++ src/features/partners/PartnersList.js
@@ -0,0 +1,31 @@
+import { Row, Col } from 'reactstrap'
+import Partner from './Partner'
+import {selectAllPartners} from './partnersSlice'
+import { useSelector } from 'react-redux'
+import Loading from '../../components/Loading'
+import Error from '../../components/Error'
+
+const PartnersList = () => {
+    const partners = useSelector(selectAllPartners);
+    const isLoading = useSelector((state) => state.partners.isLoading);
+    const errMsg = useSelector((state) => state.partners.errMsg);
+    return isLoading ? (
+        <Loading />
+    ) : errMsg ? (
+        <Error errMsg={errMsg} />
+    ) : (
+        <Col className='mt-4'>
+            <Row>
+                {partners.map((partner) => {
+                    return (
+                        <div className='d-flex mb-5' key={partner.id}>
+                            <Partner partner={partner} />
+                        </div>
+                    );
+                })}
+            </Row>
+        </Col>
+    );
+}
+
+export default PartnersList
